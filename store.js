d7e075368b0291c691e1869de3a89814ba517621
ChristopheSnacken <c.snacken@gmail.com>
2018-05-30 09:32:07 +0200

    add to basket

--- store.js
+++ store.js
@@ -1,27 +1,38 @@
 // TODO:
 // var products = []
 var products = [
-  { referenceNumber: 0001, name: "Is it worth it?", price: 10 },
-  { referenceNumber: 0002, name: "Get over it", price: 20 },
-  { referenceNumber: 0003, name: "Candle", price: 30 },
-  { referenceNumber: 0004, name: "backpain cushion", price: 30 },
-  { referenceNumber: 0005, name: "Vegan Yoga T-shirt", price: 200 },
-  { referenceNumber: 0006, name: "The plush yoga pants", price: 300 },
-  { referenceNumber: 0007, name: "Bring Yoga out of your Life", price: 30 },
-  { referenceNumber: 0008, name: "Darkness in Yoga", price: 10 }
+  { referenceNumber: 1001, name: "Is it worth it?", price: 10 },
+  { referenceNumber: 1002, name: "Get over it", price: 20 },
+  { referenceNumber: 1003, name: "Candle", price: 30 },
+  { referenceNumber: 1004, name: "Backpain cushion", price: 30 },
+  { referenceNumber: 1005, name: "Vegan Yoga T-shirt", price: 200 },
+  { referenceNumber: 1006, name: "The plush yoga pants", price: 300 },
+  { referenceNumber: 1007, name: "Bring Yoga out of your Life", price: 30 },
+  { referenceNumber: 1008, name: "Darkness in Yoga", price: 10 }
 ]
 // Declare `shoppingCart`, something where you will be storing all products that the user buys.
-var shoppingCart = ""
+var shoppingCart = []
 // Declare `products`, the different that you will be selling under each of the departments.
 
 var shopFromStore =  function () {
   var refNr = askUserForReferenceNumber();
+
   // Add the product with the matching referenceNumber to the shoppingCart
+  console.log(refNr)
+
+  function check(checkRefNr) {
+    return checkRefNr.referenceNumber == refNr
+  }
+  var received = products.find(check)
+  console.log(received)
+  shoppingCart.push(received)
+  console.log(shoppingCart)
+
 
   displayProductsFromShoppingCart();
 
   // calculate the total price of your cart, and use it:
-  displayTotalPrice(/*The variable holding the totol price*/);
+  displayTotalPrice(/*The variable holding the total price*/);
 
   // Ask the user if they want to continue shopping,
   // if yes, call this function again
@@ -35,8 +46,10 @@ var displayProductsFromShoppingCart = function() {
 };
 
 var askUserForReferenceNumber = function() {
-  // Use window.prompt to ask the user a question and capture their response,
-  // then, return the response from this function back to our caller
+// Use window.prompt to ask the user a question and capture their response,
+  var inputNumber = prompt ('What is your reference number?')
+// then, return the response from this function back to our caller
+  return  inputNumber;
 };
 
 //
